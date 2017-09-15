a51bd8ef5cc55ab04a9fa9d5f904b1a5530929d1
Van Quach <dinhvan5481@gmail.com>
2017-09-14 23:36:50 -0500

    Added missed hw2, part 1

--- hw2/python/log-file-analysis-lab/compute_highest_affinity.py
+++ hw2/python/log-file-analysis-lab/compute_highest_affinity.py
@@ -5,9 +5,31 @@
 # the user who visited the site.
 
 # See the test cases for more details.
+import operator
+
 
 def highest_affinity(site_list, user_list, time_list):
   # Returned string pair should be ordered by dictionary order
   # I.e., if the highest affinity pair is "foo" and "bar"
-  # return ("bar", "foo"). 
-  return ('abc', 'def')
+  # return ("bar", "foo").
+  result = {}
+  su_list = list(zip(site_list, user_list))
+
+  for index, su in enumerate(su_list):
+    for i in range(index + 1, len(su_list)):
+      su_next = su_list[i]
+      if su_next[1] == su[1]:
+        key_entry = (su[0], su_next[0])
+        if key_entry in result.keys():
+          result[key_entry] = result[key_entry] + 1
+        else:
+          result[key_entry] = 1
+
+  sorted_result = sorted(result.items(), key=operator.itemgetter(1), reverse=True)
+  return sorted_result[0][0]
+
+if __name__ == '__main__':
+  site_list = ["a.com", "b.com", "a.com", "b.com", "a.com", "c.com"]
+  user_list = ["andy", "andy", "bob", "bob", "charlie", "charlie"]
+  time_list = [1238972321, 1238972456, 1238972618, 1238972899, 1248472489, 1258861829]
+  highest_affinity(site_list, user_list, time_list)
